441d58eb1ba0a6a1a8260176699be486e4f5d6f2
Alfredo González Martínez <48945157+alfredogonzalezmartinez@users.noreply.github.com>
2023-11-04 11:29:16 +0100

    Handle routes

--- app/main.js
+++ app/main.js
@@ -5,8 +5,9 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("data", () => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  socket.on("data", (data) => {
+    const response = getResponse(data);
+    socket.write(response);
     socket.end();
   });
 
@@ -17,3 +18,41 @@ const server = net.createServer((socket) => {
 });
 
 server.listen(4221, "localhost");
+
+/**
+ * @typedef {Object} HttpRequest
+ * @property {string} httpMethod
+ * @property {string} target
+ * @property {string} httpVersion
+ * @property {string[]} headers
+ * @property {string} body
+ */
+
+/**
+ * @param {Buffer} data
+ * @returns {HttpRequest}
+ */
+function parseRequestData(data) {
+  const request = data.toString();
+  const [head, body] = request.split("\r\n\r\n");
+  const [startLine, ...headers] = head.split("\r\n");
+  const [httpMethod, target, httpVersion] = startLine.split(" ");
+
+  return {
+    httpMethod,
+    target,
+    httpVersion,
+    headers,
+    body,
+  };
+}
+
+/**
+ * @param {Buffer} data
+ * @returns {string}
+ */
+function getResponse(data) {
+  const { target } = parseRequestData(data);
+  if (target === "/") return "HTTP/1.1 200 OK\r\n\r\n";
+  return "HTTP/1.1 404 Not Found\r\n\r\n";
+}
